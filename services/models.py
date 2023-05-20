62505efff2fd56117dfe03148bff72d226451cc4
George Dudaev <georgeduday@gmail.com>
2023-05-20 12:48:13 +0300

    create SubscriptionSerializer and SubscriptionView

--- services/models.py
+++ services/models.py
@@ -8,6 +8,9 @@ class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'{self.name}: {self.full_price}'
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -19,8 +22,14 @@ class Plan(models.Model):
     discount_percents = models.PositiveIntegerField(default=0,
                                                     validators=[MaxValueValidator(100)])
 
+    def __str__(self):
+        return f'{self.plan_type}: -{self.discount_percents}%'
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.client} - {self.service}'
