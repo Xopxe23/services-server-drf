049f2fd48fc67cf7b70bf7c700bbf4dbf4be1525
George Dudaev <georgeduday@gmail.com>
2023-05-26 11:29:26 +0300

    create celery app, task; install redis

---
+++ celery_app.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+
+app = Celery('core')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+
+@app.task()
+def debug_task():
+    time.sleep(5)
+    print("hello from celery")
--- core/settings.py
+++ core/settings.py
@@ -136,3 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery
+
+CELERY_BROKER_URL = "redis://localhost:6379"
